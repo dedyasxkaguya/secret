4ad63d858dcf08bdf7661ea934379cf5c84e79dc
dedyasxkaguya <dedysgithub@gmail.com>
2025-10-05 02:54:39 +0700

    third test

--- script/script.js
+++ script/script.js
@@ -1,10 +1,12 @@
 const heartBox = document.getElementById("heartBox")
+const btn = document.querySelectorAll(".nextBtn")
+const prevBtn = document.querySelectorAll(".prevBtn")
 const hello = () => {
     swal.fire({
         icon: 'question',
         title: 'Hello',
         text: 'Are you someone?',
-        confirmButtonText:'Yes i Am ded'
+        confirmButtonText: 'Yes i Am ded'
     })
 }
 // hello()
@@ -18,22 +20,22 @@ const addHeart = () => {
     let animationDuration
     if (random <= 25) {
         animationDuration = random + 15
-    } else if (random >= 40 && random<=60) {
+    } else if (random >= 40 && random <= 60) {
         animationDuration = random - 25
-    } else if (random >= 60 && random<=80) {
+    } else if (random >= 60 && random <= 80) {
         animationDuration = random - 45
     } else if (random >= 80) {
         animationDuration = random - 55
-    }else{
-        animationDuration=random
+    } else {
+        animationDuration = random
     }
     let color
     if (random > 50) {
         color = 'text-blue-800'
-        zIndex=1
+        zIndex = 1
     } else {
         color = 'text-red-600'
-        zIndex=3
+        zIndex = 3
     }
     let heart = document.createElement('i')
     heart.classList.add("bi")
@@ -44,9 +46,9 @@ const addHeart = () => {
     heart.style.left = `${y}%`
     heart.style.bottom = `${x}%`
     heart.style.scale = scale
-    heart.style.zIndex=zIndex
-    heart.style.animation = `up ${animationDuration}s ease-in-out infinite`
-    heart.style.animationDelay = `${random/10}`
+    heart.style.zIndex = zIndex
+    heart.style.animation = `up ${animationDuration}s ease-in-out forwards`
+    heart.style.animationDelay = `${random / 10}`
 
     heartBox.appendChild(heart)
 }
@@ -56,3 +58,40 @@ while (i <= 87) {
     addHeart()
     i++
 }
+btn.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        try {
+            let parent = e.target.parentElement.parentElement
+            let parentSibling = e.target.parentElement.parentElement.nextElementSibling
+            if (parentSibling !== null) {
+                console.log(parentSibling)
+                parent.style.display = 'none'
+                parentSibling.style.display = 'flex'
+            } else {
+
+                swal.fire({
+                    title: "Oh No 🥺",
+                    html: 'Its already the end of the message <br> Would you like to continue 🦌',
+                    confirmButtonText: "Yes i do"
+                })
+
+            }
+        } catch {
+            console.error("Tidak bisa jendral")
+        }
+    })
+})
+prevBtn.forEach((btn)=>{
+    btn.addEventListener("click",(e)=>{
+        try{
+            let parent = e.target.parentElement.parentElement
+            let parentSibling = e.target.parentElement.parentElement.previousElementSibling
+            if(parent!==null){
+                parent.style.display='none'
+                parentSibling.style.display='flex'
+            }
+        }catch{
+            console.error("Tidak bisa jendral")
+        }
+    })
+})
